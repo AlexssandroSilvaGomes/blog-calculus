5702c35955d686eab5772a767fd383ed16e450ad
unknown <alexssandrosilva244@gmail.com>
2023-12-08 23:01:52 -0300

    teste

--- src/pages/derivada/Derivada.jsx
+++ src/pages/derivada/Derivada.jsx
@@ -4,10 +4,10 @@ import Header from '../../ui/components/header/Header'
 import Footer from '../../ui/components/footer/Footer'
 import Banner from '../../ui/components/banner/Banner'
 import Paragraph from '../../ui/components/paragraph/Paragraph'
-import derivada from './img/derivada.png'
+/* import derivada from './img/derivada.png'
 import derivada1 from './img/derivada1.png'
 import derivada2 from './img/derivada3.png'
-import derivada3 from './img/exercicio-resolvido-derivada-quociente.png'
+import derivada3 from './img/exercicio-resolvido-derivada-quociente.png' */
 
 
 function Derivada() {
@@ -19,12 +19,12 @@ function Derivada() {
         <div className="derivadas">
           <div className='derivadas-texto'>
             <Paragraph title="Definição" content="Se uma função f é definida em um intervalo aberto contendo x0, então a derivada de f em x0, denotada por f'(x0), é dada por: " />
-            <img src={derivada} />
+            {/* <img src={derivada} /> */}
             <Paragraph content="se este limite existir. &#916;x representa uma pequena variação em x, próximo de x0, ou seja, tomando x = x0 + &#916;x (&#916;x = x − x) , a derivada de f em x0 pode também se expressa por " />
-            <img src={derivada1} />
+            {/* <img src={derivada1} /> */}
             <div className='notacoes'>
               <p>Notações:</p>
-              <img src={derivada2} />
+              {/* <img src={derivada2} /> */}
             </div>
             <div className='regras'>
               <Paragraph title="Regras de Derivação" content="Sejam f (x) e g (x) funções deriváveis e seja a um número real qualquer. Então, valem as propriedades: " />
@@ -36,7 +36,7 @@ function Derivada() {
               <Paragraph content="vi) (Regra do produto) [f (x) g (x)]' = f ' (x) g (x) + f (x) g' (x)." />
               <Paragraph content="vii) (regra do quociente):" />
             </div>
-            <img src={derivada3} />
+            {/* <img src={derivada3} /> */}
           </div>
 
           <div className="derivadas-video">
--- src/pages/integral/Integral.jsx
+++ src/pages/integral/Integral.jsx
@@ -4,13 +4,13 @@ import Header from '../../ui/components/header/Header'
 import Footer from '../../ui/components/footer/Footer'
 import Banner from '../../ui/components/banner/Banner'
 import Paragraph from '../../ui/components/paragraph/Paragraph'
-import integralIndefinida from './img/integral-indefinida.png'
+/* import integralIndefinida from './img/integral-indefinida.png'
 import integralIndefinida1 from './img/integral-indefinida1.gif'
 import integralIndefinida2 from './img/integral-indefinida2.gif'
 import integralIndefinida3 from './img/integral-indefinida3.gif'
 import integralDefinida from './img/integral-definida.png'
 import integralDefinida1 from './img/integral-definida1.gif'
-import integralDefinidaSemC from './img/integral-definida-semC.png'
+import integralDefinidaSemC from './img/integral-definida-semC.png' */
 
 function Integral() {
   return (
@@ -22,7 +22,7 @@ function Integral() {
           <div className='secao-indefinida'>
             <Paragraph title="Integral Indefinida" content="Uma integral é dita indefinida quando não se conhece os limites de integração, ou seja, o intervalo no qual ela está sendo integrada." />
             <p>Exemplo:</p>
-            <img src={integralIndefinida} />
+            {/* <img src={integralIndefinida} /> */}
             <Paragraph content="Na integração indefinida, a função resultante será a função integrada F(x), sendo necessário somá-la a uma constante, chamada de constante de integração" />
             <p>onde:</p>
             <ul>
@@ -44,17 +44,17 @@ function Integral() {
             <ul className="listIntegrais">
               <li>
                 <p>1°:</p>
-                <img className='img1' src={integralIndefinida1} />
+                {/* <img className='img1' src={integralIndefinida1} /> */}
                 <Paragraph content=", ou seja, a integral da soma ou diferença é a soma ou diferença das integrais." />
               </li>
               <li>
                 <p>2°:</p>
-                <img className='img1' src={integralIndefinida2} />
+                {/* <img className='img1' src={integralIndefinida2} /> */}
                 <Paragraph content=", ou seja, a constante multiplicativa pode ser retirada do integrando." />
               </li>
               <li>
                 <p>3°:</p>
-                <img className='img1' src={integralIndefinida3} />
+                {/* <img className='img1' src={integralIndefinida3} /> */}
                 <Paragraph content=", ou seja, a derivada da integral de uma função é a própria função." />
               </li>
             </ul>
@@ -69,11 +69,11 @@ function Integral() {
           <div className='secao-definida'>
             <Paragraph title="Integral Definida" content="Diferentemente da integral indefinida, os limites da integral definida já estão estabelecidos, para resolvê-la, basta encontrar a integral da função em questão, e neste resultado substituir os valores dos limites superior e inferior. " />
             <p>Exemplo:</p>
-            <img src={integralDefinida} />
+            {/* <img src={integralDefinida} /> */}
             <Paragraph content="Como as constantes de integração são iguais, a integral definida é a subtração das funções primitivas substituídas pelos limites superior e inferior, neste caso (B e A respectivamente). Resultando em:" />
-            <img src={integralDefinidaSemC} />
+            {/* <img src={integralDefinidaSemC} /> */}
             <Paragraph title="Propriedades da Integral Definida" />
-            <img className='last-img' src={integralDefinida1} />
+           {/*  <img className='last-img' src={integralDefinida1} /> */}
           </div>
 
           <div className="video-definida">
