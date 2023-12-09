bfcc34846bcda6ba7d3f8e3eaa241a3ec813f22d
unknown <alexssandrosilva244@gmail.com>
2023-12-08 22:47:32 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import imagePresets, { widthPreset } from 'vite-plugin-image-presets'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    imagePresets({
+      thumbnail: widthPreset({
+        class: 'img thumb',
+        loading: 'lazy',
+        widths: [48, 96],
+        formats: {
+          webp: { quality: 50 },
+          jpg: { quality: 70 },
+        },
+      }),
+    }),
+  ],
 })
